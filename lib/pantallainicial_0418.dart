e2e255863b2f34623769ecd12e1996c02c248f49
UribeV128 <143779478+UribeV128@users.noreply.github.com>
2024-03-22 13:13:13 -0600

    Update pantallainicial_0418.dart

--- lib/pantallainicial_0418.dart
+++ lib/pantallainicial_0418.dart
@@ -10,7 +10,7 @@ class PantallaInicial_0418 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Pantalla Inicial Uribe 0418"),
-        backgroundColor: Colors.black,
+        backgroundColor: Color(0xff8fbde3),
       ),
       body: Center(
           child: Column(
@@ -20,13 +20,97 @@ class PantallaInicial_0418 extends StatelessWidget {
             onPressed: () {
               Navigator.pushNamed(context, "/Pantalla1_0418");
             },
-            child: const Text("Mover a pantalla 1"),
+            child: const Text("Zona de aterrizaje p1"),
           ),
           ElevatedButton(
             onPressed: () {
               Navigator.pushNamed(context, "/Pantalla2_0418");
             },
             child: const Text("Mover a pantalla 2"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla3_0418");
+            },
+            child: const Text("Mover a pantalla 3"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla4_0418");
+            },
+            child: const Text("Mover a pantalla 4"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla5_0418");
+            },
+            child: const Text("Mover a pantalla 5"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla6_0418");
+            },
+            child: const Text("Mover a pantalla 6"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla7_0418");
+            },
+            child: const Text("Mover a pantalla 7"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla8_0418");
+            },
+            child: const Text("Mover a pantalla 8"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla9_0418");
+            },
+            child: const Text("Mover a pantalla 9"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla10_0418");
+            },
+            child: const Text("Mover a pantalla 10"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla11_0418");
+            },
+            child: const Text("Mover a pantalla 11"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla12_0418");
+            },
+            child: const Text("Mover a pantalla 12"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla13_0418");
+            },
+            child: const Text("Mover a pantalla 13"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla14_0418");
+            },
+            child: const Text("Mover a pantalla 14"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla15_0418");
+            },
+            child: const Text("Mover a pantalla 15"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, "/Pantalla16_0418");
+            },
+            child: const Text("Mover a pantalla 16"),
           ),
         ], //ninos
       )),
